6870eb26250d86fdfdef44624959f91fb5b79ca6
Ellie Bahadori <elliedori@gmail.com>
2020-09-03 14:39:57 -0700

    Update script

--- index.js
+++ index.js
@@ -1,109 +1,110 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 const https = require('https');
 const glob = require("glob");
 
-try {
-  const slackWebhookUrl = core.getInput('slack-webhook-url');
-  const branchName = core.getInput('branch-name');
-  const commitSha = core.getInput('commit-sha');
-  const commitOwner = core.getInput('commit-owner');
-  const repoName = core.getInput('repo-name');
-  const runId = core.getInput('run-id');
+const slackWebhookUrl = core.getInput('slack-webhook-url');
+const branchName = core.getInput('branch-name');
+const commitSha = core.getInput('commit-sha');
+const commitOwner = core.getInput('commit-owner');
+const repoName = core.getInput('repo-name');
+const runId = core.getInput('run-id');
 
-  // cat all error files in environment
-  // build Slack message
-  // send Slack message
-
-  const userAccountNotification = {
-    "blocks":[
-      {
-          "type":"header",
-          "text":{
-            "type":"plain_text",
-            "text":`CI build failed for commit ${commitSha} on ${branchName}`
-          }
-      },
-      {
-          "type":"section",
-          "text":{
-            "type":"mrkdwn",
-            "text":`— _Author: ${commitOwner}_  <https://github.com/${repoName}/actions/runs/${runId}|View build>`
-          }
-      },
-      {
-          "type":"section",
-          "text":{
-            "type":"mrkdwn",
-            "text":"\n <results go here>"
-          }
-      }
-    ]
-  };
-
-  const errorOutput = ""
-
-  const errorFiles = glob("job-*.txt", function (er, files) {
-    if (er) { throw new Error("Unable to parse files") }
-    return files;
-  })
-
-  console.log("error files are: ", errorFiles)
-
-  for (var file in errorFiles) {
-    errorOutput += fs.readFileSync(file, 'utf8');
-  }
-
-  console.log("Final error output is: ", errorOutput);
-
-  function sendSlackMessage (webhookURL, messageBody) {
-    try {
-      messageBody = JSON.stringify(messageBody);
-    } catch (e) {
-      throw new Error('Failed to stringify messageBody', e);
-    }
+function prepareErrorOutput() {
+  return new Promise((resolve, reject) => {
+    glob("job-*.txt", {nonull: true}, function (err, files) {
+      if (err) { reject("Unable to parse files") }
   
-    return new Promise((resolve, reject) => {
-      const requestOptions = {
-        method: 'POST',
-        header: {
-          'Content-Type': 'application/json'
+      fullErrors = ""
+      for (let i=0; i<files.length; i++) {
+        try {
+          fullErrors += fs.readFileSync(files[i], 'utf8')
+        } catch (err) {
+          reject("Failed to read", err)
         }
-      };
-  
-      const req = https.request(webhookURL, requestOptions, (res) => {
-        let response = '';
-  
-        res.on('data', (d) => {
-          response += d;
-        });
-        res.on('end', () => {
-          resolve(response);
-        })
+      }
+      resolve(fullErrors);
+    })
+  })
+}
+
+function userAccountNotification (errors) {
+  return {"blocks":[
+    {
+        "type":"header",
+        "text":{
+          "type":"plain_text",
+          "text":`CI build failed for commit ${commitSha} on ${branchName}`
+        }
+    },
+    {
+        "type":"section",
+        "text":{
+          "type":"mrkdwn",
+          "text":`— _Author: ${commitOwner}_  <https://github.com/${repoName}/actions/runs/${runId}|View build>`
+        }
+    },
+    {
+        "type":"section",
+        "text":{
+          "type":"mrkdwn",
+          "text":`\n ${errors}`
+        }
+    }
+  ]}
+};
+
+async function makePostRequest(webhookUrl, messageBody) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const req = https.request(webhookUrl, options, res => {
+      let response = '';
+      res.on('data', d => {
+        response += d;
       });
-  
-      req.on('error', (e) => {
-        reject(e);
+      res.on('end', () => {
+        resolve(response);
       });
-  
-      req.write(messageBody);
-      req.end();
     });
+
+    req.on('error', err => {
+      reject(err)
+    })
+
+    req.write(JSON.stringify(messageBody));
+    req.end();
+  });
+};
+
+async function sendSlackNotification(message) {
+  if (!slackWebhookUrl) {
+    console.error('Please supply a Slack webhool URL');
   }
-  
-  (async function () {
-    if (!slackWebhookUrl) {
-      console.error('Please supply a Slack webhool URL');
-    }
-  
+
+  try {
     console.log('Sending slack message');
-    try {
-      const slackResponse = await sendSlackMessage(slackWebhookUrl, userAccountNotification);
-      console.log('Message response', slackResponse);
-    } catch (e) {
-      console.error('There was a error with the request', e);
-    }
-  })();
+    const slackResponse = await makePostRequest(slackWebhookUrl, message);
+    console.log('Message response', slackResponse);
+  } catch (e) {
+    console.error('There was a error with the request', e);
+  }
+};
+
+async function prepareAndSendNotification() {
+  const errors = await prepareErrorOutput()
+  const slackMessage = userAccountNotification(errors)
+  await sendSlackNotification(slackMessage);
+}
+
+try {
+
+  prepareAndSendNotification();
 
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
